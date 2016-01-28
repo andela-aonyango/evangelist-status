74b000cd88e8b0c33aeb6680d2c58f5cad4d5c72
andela-aonyango <andela-aonyango@users.noreply.github.com>
2016-01-28 15:37:00 +0300

    Added comments to explain what the tests were targetting

--- tests/EvangelistStatusTest.php
+++ tests/EvangelistStatusTest.php
@@ -11,15 +11,19 @@ class EvangelistStatusTest extends \PHPUnit_Framework_TestCase
     */
     public function testGetStatus()
     {
+        // test with a user who has less than 5 public repos
         $status = new EvangelistStatus("andela-aomondi");
         $this->assertEquals("You call yourself a programmer?!!", $status->getStatus());
 
+        // test with a user who has 5-10 public repos
         $status = new EvangelistStatus("andela-jngatia");
         $this->assertEquals("Prodigal Junior Evangelist", $status->getStatus());
 
+        // test with a user who has 11-20 public repos
         $status = new EvangelistStatus("andela-cganga");
         $this->assertEquals("Associate Evangelist", $status->getStatus());
 
+        // test with a user who has > 20 public repos
         $status = new EvangelistStatus("vonmises");
         $this->assertEquals("Senior Evangelist", $status->getStatus());
     }
