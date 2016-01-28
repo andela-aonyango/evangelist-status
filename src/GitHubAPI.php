8c64a9783f0b35262471a19b1057814cfc12a2d5
andela-aonyango <andela-aonyango@users.noreply.github.com>
2016-01-28 14:50:28 +0300

    Moved the user agent from the getGitHubInfo method and added comments

--- src/GitHubAPI.php
+++ src/GitHubAPI.php
@@ -2,23 +2,46 @@
 
 namespace Lib;
 
+use Lib\Exceptions\NullUserException;
+
 class GitHubAPI
 {
+    /**
+     * Returns the number of public repos a developer has on GitHub
+     *
+     * @param  string $username GitHub username
+     * @return mixed
+     * @throws NullUserException
+     *
+     */
     public function getNumberOfRepos($username)
     {
+        if (empty($username) || empty(trim($username)))
+        {
+            throw new NullUserException("A username is required");
+        }
+
         $url = 'https://api.github.com/users/' . $username;
-        $json = self::getGithubInfo($url);
+        $json = self::getGithubInfo($url, "evangelist-status");
         $obj = json_decode($json);
 
         return $obj->{'public_repos'};
     }
 
-    private function getGithubInfo($url) {
+    /**
+     * Returns the developer's public GitHub information in JSON
+     *
+     * @param  string $url The URL to retrieve API information from
+     * @param  string $useragent The user agent or app name required by GitHub
+     * @return string
+     *
+     */
+    private function getGithubInfo($url, $useragent) {
         $curl_handle = curl_init();
         curl_setopt($curl_handle, CURLOPT_URL, $url);
         curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1); 
         curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 10);
-        curl_setopt($curl_handle, CURLOPT_USERAGENT, "evangelist-status");
+        curl_setopt($curl_handle, CURLOPT_USERAGENT, $useragent);
         $content = curl_exec($curl_handle);
         curl_close($curl_handle);
         return $content;
