aaea088fae086b031071df16e88ba1109f54a67d
andela-aonyango <andela-aonyango@users.noreply.github.com>
2016-02-01 12:10:46 +0300

    Replaced cURL with Guzzle

--- src/GitHubAPI.php
+++ src/GitHubAPI.php
@@ -6,9 +6,13 @@
  * @author   andrew <andrew.onyango@andela.com>
  * @license  MIT => https://opensource.org/licenses/MIT
  */
+
 namespace Lib;
 
 use Lib\Exceptions\NullUserException;
+use Lib\Exceptions\UserNotFoundException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 /**
  * @category Class
@@ -19,19 +23,18 @@ class GitHubAPI
     /**
      * Returns the number of public repos a developer has on GitHub
      *
-     * @param  string $username GitHub username
+     * @param  string $username    GitHub username
      * @return mixed
      * @throws NullUserException
      */
     public static function getNumberOfRepos($username)
     {
         if (empty(trim($username))) {
-            throw new NullUserException("A username is required");
+            throw new NullUserException("A valid username is required.");
         }
 
-        $url = "https://api.github.com/users/" . $username;
-        $json = self::getGithubInfo($url, "evangelist-status");
-        $obj = json_decode($json);
+        $githubInfo = self::getGithubInfo($username);
+        $obj = json_decode($githubInfo);
 
         return $obj->{"public_repos"};
     }
@@ -39,20 +42,25 @@ class GitHubAPI
     /**
      * Returns the developer's public GitHub information in JSON
      *
-     * @param  string $url       The URL to retrieve API information from
-     * @param  string $useragent The user agent or app name required by GitHub
+     * @param  string $username    The username whose information is to be retrieved
      * @return string
+     * @throws UserNotFoundException
      */
-    private static function getGithubInfo($url, $useragent)
+    private static function getGithubInfo($username)
     {
-        // use guzzle, gouete
-        $curl_handle = curl_init();
-        curl_setopt($curl_handle, CURLOPT_URL, $url);
-        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 10);
-        curl_setopt($curl_handle, CURLOPT_USERAGENT, $useragent);
-        $content = curl_exec($curl_handle);
-        curl_close($curl_handle);
-        return $content;
+        $client = new Client(["base_uri" => "https://api.github.com/users/"]);
+
+        try {
+            $response = $client->get($username);
+            $info = $response->getBody(true);
+        } catch (ClientException $e) {
+            if ($e->getCode() === 404) {
+                throw new UserNotFoundException(
+                    "The user " . $username . " was not found on GitHub."
+                );
+            }
+        }
+
+        return $info;
     }
 }
