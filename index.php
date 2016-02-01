a8563b887f8d77d741763abe88239ca1494ff881
andela-aonyango <andela-aonyango@users.noreply.github.com>
2016-02-01 12:16:02 +0300

    Updated the example index.php

--- index.php
+++ index.php
@@ -4,7 +4,24 @@
 require "vendor/autoload.php";
 
 use Lib\EvangelistStatus;
+use Lib\Exceptions\NullUserException;
+use Lib\Exceptions\UserNotFoundException;
 
-$status = new EvangelistStatus("andela-aonyango");
+// valid and invalid users
+$users = [
+    "valid"         => "vonmises",
+    "invalid"       => "  ",
+    "non-existent"  => "jahsgdkjahsgd",
+];
 
-echo $status->getStatus();
+// how one can use this package, including exceptions
+foreach ($users as $key => $value) {
+    try {
+        $status = new EvangelistStatus($users[$key]);
+        echo $status->getStatus() . "<br>";
+    } catch (NullUserException $e) {
+        echo $e->getMessage() . "<br>";
+    } catch (UserNotFoundException $e) {
+        echo $e->getMessage() . "<br>";
+    }
+}
