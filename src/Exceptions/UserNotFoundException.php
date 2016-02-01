0202a72d746a61504c173357aae6e93cde97e897
andela-aonyango <andela-aonyango@users.noreply.github.com>
2016-02-01 11:52:45 +0300

    Added a UserNotFoundException

---
+++ src/Exceptions/UserNotFoundException.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @file     NullUserException.php
+ * This file is a class that should be used to communicate a null user
+ * error to a user.
+ * @package  Lib\EvangelistStatus
+ * @author   andrew <andrew.onyango@andela.com>
+ * @license  MIT => https://opensource.org/licenses/MIT
+ */
+
+namespace Lib\Exceptions;
+
+use Exception;
+
+/**
+ * @category Class
+ * @package  Lib\EvangelistStatus
+ */
+class UserNotFoundException extends Exception
+{
+    protected $message;
+
+    /**
+     * @param string $message
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+
+        // make sure everything is properly assigned
+        parent::__construct($message);
+    }
+}
